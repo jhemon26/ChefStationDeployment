2167748352977d3bc4ec1c00c565094708694bff
Jahid H Emon <je2264r@gre.ac.uk>
2026-04-24 20:30:47 +0100

    Restore sky blue logo style

--- client/src/components/BrandLogo.jsx
+++ client/src/components/BrandLogo.jsx
@@ -2,11 +2,7 @@ export default function BrandLogo({ centered = false }) {
   return (
     <div className={`sidebar-logo brand-logo ${centered ? 'brand-logo-centered' : ''}`}>
       <div className="logo-icon" aria-hidden="true">
-        <div className="logo-monogram">
-          <span className="logo-monogram-bar" />
-          <span className="logo-glyph logo-glyph-c">c</span>
-          <span className="logo-glyph logo-glyph-s">s</span>
-        </div>
+        <span className="logo-badge-copy">CS</span>
       </div>
       <div className="logo-text">
         <span className="logo-text-chef">Chef</span>
